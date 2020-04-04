447db7546b8377d8f9c581f2b42499908ff7f554
Ash Ryan Arnwine <accounts@ashryan.io>
2020-04-04 17:54:55 -0400

    Refactor with common getSection methoed. Moved Airtable URLs to .env. Started adding more info.

--- src/slack.js
+++ src/slack.js
@@ -1,128 +1,118 @@
-const Slack = require('slack');
-
+require("dotenv").config();
+const Slack = require("slack");
 const token = process.env.SLACK_XOXB;
 const channel = process.env.SLACK_CHANNEL_ID;
 const bot = new Slack({ token });
+const requestsURL = "https://airtable.com/tblaL1g6IzH6uPclD/viwEjCF8PkEfQiLFC/";
 
-const getErrand = record => {
-  // Prepare the general info
-  const profileURL = `https://airtable.com/tblaL1g6IzH6uPclD/viwEjCF8PkEfQiLFC/${record.id}`;
-  const header = [
-    `<${profileURL}|${record.get('Name')}>`,
-    record.get('Phone number'),
-    record.get('Address'),
+// This function actually sends the message to the slack channel
+const sendMessage = (record, volunteers) => {
+  const text = "A new errand has been added!";
+  const heading = getSection(`:exclamation: *${text}* :exclamation:`);
+  const requester = getRequester(record);
+  const tasks = getTasks(record);
+  const language = getLanguage(record);
+  const space = getSection(" ");
+  const volunteerList = getVolunteers(volunteers);
+
+  return bot.chat.postMessage({
+    token,
+    channel,
+    text,
+    blocks: [heading, requester, tasks, language, space],
+    attachments: [
+      {
+        blocks: volunteerList
+      }
+    ]
+  });
+};
+
+const getRequester = record => {
+  const recordURL = `${requestsURL}${record.id}`;
+  const textLines = [
+    `<${recordURL}|${record.get("Name")}>`,
+    record.get("Phone number"),
+    record.get("Address")
   ];
-  const errandObject = {
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: header.join('\n'),
-    },
-  };
+  const text = textLines.join("\n");
 
-  return errandObject;
+  const requesterObject = getSection(text);
+
+  return requesterObject;
 };
 
-const getVolunteers = volunteers => {
-  // Find the closest volunteers
-  const volObject = [];
+const getTasks = record => {
+  const tasks = formatTasks(record);
+  const tasksObject = getSection(`*Needs assistance with:*${tasks}`);
 
-  if (volunteers.length > 0) {
-    // Header
-    volObject.push({
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: '*Here are the 10 closest volunteers*',
-      },
-    });
-
-    // Prepare the verbose volunteer info
-    volunteers.forEach((volunteer) => {
-      const volunteerURL = `https://airtable.com/tblxqtMAabmJyl98c/viwNYMdylPukGiOYQ/${volunteer.record.id}`;
-      volObject.push({
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `<${volunteerURL}|${volunteer.Name}> - ${volunteer.Number} - ${volunteer.Distance.toFixed(2)} Mi.`,
-        },
-      });
-    });
-
-    const msg = 'Here are the volunteer phone numbers for easy copy/pasting:';
-    volObject.push({
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: [msg].concat(volunteers.map((v) => v.Number)).join('\n'),
-      },
-    });
-  } else {
-    // No volunteers found
-    volObject.push({
-      type: 'section',
-      text: { type: 'mrkdwn', text: '*No volunteers match this request!*\n*Check the full Airtable record, there might be more info there.*' },
-    });
-  }
-
-  return volObject;
+  return tasksObject;
 };
 
-// Gets list of tasks from spreadsheet and adds to message text
-const formatTasks = row => {
-  let formattedTasks = '';
+const getLanguage = record => {
+  const language = record.get("Language");
+  const languageObject = getSection(`*Speaks:* ${language}`);
 
-  const tasks = row.get('Tasks');
+  return languageObject;
+};
+
+const formatTasks = record => {
+  let formattedTasks = "";
+  const tasks = record.get("Tasks");
+
+  // Put each task on a new line
   if (tasks) {
-    formattedTasks = row.get('Tasks').reduce((msg, task) => `${msg}\n :small_orange_diamond: ${task}`, '');
+    formattedTasks = record
+      .get("Tasks")
+      .reduce((msg, task) => `${msg}\n :small_orange_diamond: ${task}`, "");
   }
 
   return formattedTasks;
 };
 
-const getTask = record => {
-  // Process the requested tasks
-  const taskObject = {
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: `*Needs assistance with:*${formatTasks(record)}`,
-    },
-  };
+const getVolunteers = volunteers => {
+  const volObject = [];
 
-  return taskObject;
+  if (volunteers.length > 0) {
+    // Heading for volunteers
+    volObject.push(getSection("*Here are the 10 closest volunteers*"));
+
+    // Prepare the detailed volunteer info
+    volunteers.forEach(volunteer => {
+      const volunteerURL = `https://airtable.com/tblxqtMAabmJyl98c/viwNYMdylPukGiOYQ/${volunteer.record.id}`;
+      const volunteerText = `<${volunteerURL}|${volunteer.Name}> - ${
+        volunteer.Number
+      } - ${volunteer.Distance.toFixed(2)} Mi.`;
+
+      volObject.push(getSection(volunteerText));
+    });
+
+    // Add phone number list for copy/paste
+    const msg = "Here are the volunteer phone numbers for easy copy/pasting:";
+    const phoneText = [msg].concat(volunteers.map(v => v.Number)).join("\n");
+
+    volObject.push(getSection(phoneText));
+  } else {
+    // No volunteers found
+    const noneFoundText =
+      "*No volunteers match this request!*\n*Check the full Airtable record, there might be more info there.*";
+
+    volObject.push(getSection(noneFoundText));
+  }
+
+  return volObject;
 };
 
-// This function actually sends the message to the slack channel
-const sendMessage = (record, volunteers) => {
-  const errand = getErrand(record);
-  const task = getTask(record);
-  const vols = getVolunteers(volunteers);
-
-  return bot.chat.postMessage({
-    token,
-    channel,
-    text: '',
-    blocks: [{
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: ':exclamation: *A new errand has been added!* :exclamation:',
-      },
-    },
-    errand,
-    task,
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: ' ',
-      },
-    }],
-    attachments: [{ blocks: vols }],
-  });
+const getSection = text => {
+  return {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text
+    }
+  };
 };
 
 module.exports = {
-  sendMessage,
+  sendMessage
 };
