3d173195b52d1107a595c34191e64f7069e87178
Ash Ryan Arnwine <accounts@ashryan.io>
2020-04-26 00:02:30 -0400

    ESLint

--- test/slack/sendDispatch.test.js
+++ test/slack/sendDispatch.test.js
@@ -1,19 +1,7 @@
 const { sendDispatch } = require("../../src/slack/sendDispatch");
 
-const mockRequestRecord = {
-  get(field) {
-    return this.fields[field];
-  },
-  set(field, value) {
-    return (this.fields[field] = value);
-  },
-  fields: {},
-};
-
 test("sendMessage() throws error when no requester record is passed", async () => {
-  try {
-    await sendDispatch();
-  } catch (error) {
-    expect(error.message).toMatch("No record passed to sendMessage().");
-  }
+  await expect(sendDispatch()).rejects.toThrow(
+    "No record passed to sendMessage()."
+  );
 });
