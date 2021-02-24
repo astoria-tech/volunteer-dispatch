68278229ea8fe328b98adfdab9811ccf840f141e
Peter Valdez <peter@shipyard.build>
2021-02-24 02:44:34 -0500

    Correctly use the new array format of the language column

--- src/languageFilter.js
+++ src/languageFilter.js
@@ -4,19 +4,32 @@
  * @returns {Array} List of potential volunteers that speak requester's language.
  */
 function filterByLanguage(request, volunteerDistances) {
-  return request.get("Language") && request.get("Language") !== "English"
-    ? volunteerDistances.filter((volunteerAndDistance) => {
-        const volunteer = volunteerAndDistance[0];
-        const volLanguages = volunteer.get(
-          "Please select any language you have verbal fluency with:"
-        );
-        if (volLanguages) {
-          return volLanguages.some(
-            (language) => language === request.get("Language")
-          );
-        }
-      })
-    : volunteerDistances;
+  // HACK: only use the first language until we introduce logic to search for multiple languagers
+  let firstLanguage;
+  const languages = request.get("Language");
+  if (languages && languages.length && languages.length > 0) {
+    firstLanguage = languages.shift();
+  } else {
+    firstLanguage = "English";
+  }
+
+  // If the language is English, return everyone. Otherwise, filter by the language
+  let result;
+  if (firstLanguage === "English") {
+    result = volunteerDistances;
+  } else {
+    result = volunteerDistances.filter((volunteerAndDistance) => {
+      const volunteer = volunteerAndDistance[0];
+      const volLanguages = volunteer.get(
+        "Please select any language you have verbal fluency with:"
+      );
+      return (volLanguages || []).some(
+        (language) => language === firstLanguage
+      );
+    });
+  }
+
+  return result;
 }
 
 module.exports = { filterByLanguage };
